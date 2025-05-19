37f61c48f80a2130a8463a35ec93945d4227b57a
Erik Herazo <eherazo@lsv-tech.com>
2025-05-19 14:52:00 -0500

    test con error

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,6 @@ def test_greet():
 def test_multiplicacion_dos_numeros():
     assert multiplicacion_dos_numeros(2, 3) == 6
     assert multiplicacion_dos_numeros(-1, 5) == -5
-    assert multiplicacion_dos_numeros(0, 10) == 0
-    assert multiplicacion_dos_numeros(5, 0) == 0
-    assert multiplicacion_dos_numeros(1, 1) == 1
-    assert multiplicacion_dos_numeros(-2, -3) == 10
+
+def test_multiplicacion_dos_numeros_error():
+    assert multiplicacion_dos_numeros(2, 3) == 5
